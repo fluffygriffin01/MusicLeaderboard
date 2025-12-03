70d8a13e76c5ffd5d41e834e5a98af76e244ce65
fluffygriffin01 <urenajulian084@gmail.com>
2025-12-02 17:09:00 -0800

    Implemented CRUD operations for artist leaderboard.

--- app.py
+++ app.py
@@ -3,20 +3,49 @@ import leaderboard
 
 app = Flask(__name__)
 
-@app.route('/api/artist_leaderboard', methods=['GET'])
-def get_artist_leaderboard():
-    response = leaderboard.get_top_ten_artists()
-    return jsonify(response)
-
-@app.route('/api/add_artist', methods=['POST'])
-def add_artist():
-    data = request.json
+@app.route('/api/artist_leaderboard/10', methods=['GET'])
+def get_top_ten_artists():    
     try:
-        leaderboard.add_artist(data.get('name'), data.get('score'))
-        return jsonify({"received": data, "status": "success"})
+        response = leaderboard.get_top_artists(10)
+        return jsonify(response)
     except Exception as e:
-        print(f"Could not add artist: {e}")
-        return jsonify({"received": data, "status": "failed"})
+        return jsonify({"Failed to retrieve artist leaderboard": 500, "Error": str(e)})
+
+@app.route('/api/artist_leaderboard/create', methods=['POST'])
+def create_artist():
+    try:
+        data = request.json
+        artists_added = leaderboard.add_artist(data.get('name'), data.get('score'))
+        if artists_added > 0:
+            return jsonify({"Succssessfully created artist": 200})
+        else:
+            raise Exception("Failed to create artist.")
+    except Exception as e:
+        return jsonify({"Failed to create artist": 400, "Error": str(e)})
+    
+@app.route('/api/artist_leaderboard/update', methods=['PUT'])
+def update_artist():
+    try:
+        data = request.json
+        artists_updated = leaderboard.update_artist(data.get('name'), data.get('score'))
+        if artists_updated > 0:
+            return jsonify({"Succssessfully updated artist": 200})
+        else:
+            raise Exception("Failed to update artist.")
+    except Exception as e:
+        return jsonify({"Failed to update artist": 400, "Error": str(e)})
+    
+@app.route('/api/artist_leaderboard/delete', methods=['DELETE'])
+def delete_artist():
+    try:
+        data = request.json
+        artists_deleted = leaderboard.remove_artist(data.get('name'))
+        if artists_deleted > 0:
+            return jsonify({"Succssessfully deleted artist": 200})
+        else:
+            return jsonify({"Artist not found": 200})
+    except Exception as e:
+        return jsonify({"Failed to delete artist": 400, "Error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True, port=6000)
