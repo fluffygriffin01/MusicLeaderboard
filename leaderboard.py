699b4a7921c8774daafe2b9e0bb40818751e75d9
fluffygriffin01 <urenajulian084@gmail.com>
2025-12-02 17:09:00 -0800

    Implemented CRUD operations for artist leaderboard.

--- leaderboard.py
+++ leaderboard.py
@@ -2,12 +2,17 @@ import redis
 
 client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-def get_top_ten_artists():
-    client.zadd('artist_leaderboard', {'Bob Bobson': 234})
-    return client.zrevrange('artist_leaderboard', 0, 9, withscores=True)
+def get_top_artists(size):
+    return client.zrevrange('artist_leaderboard', 0, size - 1, withscores=True)
 
 def add_artist(name, score):
-    client.zadd('artist_leaderboard', {name: score})
+    return client.zadd('artist_leaderboard', {name: score}, nx=True)
+    
+def update_artist(name, score):
+    return client.zadd('artist_leaderboard', {name: score}, xx=True, ch=True)
+
+def remove_artist(name):
+    return client.zrem('artist_leaderboard', name)
 
 
 # pip install redis    
