c147eb1b4763999326c91e956a23e96eb1804fc0
fluffygriffin01 <urenajulian084@gmail.com>
2025-12-06 15:36:56 -0800

    Added seed_artists function

--- seeder.py
+++ seeder.py
@@ -1,4 +1,5 @@
 from faker import Faker
+import controller
 
 fake = Faker()
 
@@ -6,3 +7,12 @@ def generate_artist():
     name = fake.name()
     email = fake.email()
     return {"name": name, "email": email}
+
+def seed_artists(count):
+    added_amount = 0
+    for _ in range(count):
+        artist_info = generate_artist()
+        added_artist = controller.add_artist(artist_info['name'], artist_info['email'])
+        if added_artist == True:
+            added_amount += 1
+    return added_amount
