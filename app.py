06b69103d6907187f699870297c856bdc7521ad2
fluffygriffin01 <urenajulian084@gmail.com>
2025-12-06 15:43:12 -0800

    Updated API function outputs to be formatted correctly.

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 @app.route('/api/artist/seed/100', methods=['POST'])
 def seed_artists():    
     try:
-        response = seeder.seed_artists(100)
-        return jsonify({'Successfully seeded 100 artists': 200})
+        count = seeder.seed_artists(100)
+        return jsonify({
+            'message': 'Successfully seeded artists',
+            'count': count
+        }), 200
     except Exception as e:
-        return jsonify({'Failed to seed artists': 500, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to seed artists',
+            'error': str(e)
+        }), 400
     
 @app.route('/api/artist/leaderboard/10', methods=['GET'])
 def get_top_ten_artists():    
@@ -18,7 +24,10 @@ def get_top_ten_artists():
         response = controller.get_top_artists(10)
         return jsonify(response)
     except Exception as e:
-        return jsonify({'Failed to retrieve artist leaderboard': 500, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to retrieve artist leaderboard',
+            'error': str(e)
+        }), 500
     
 @app.route('/api/artist/id', methods=['GET'])
 def get_artist_by_id():    
@@ -27,7 +36,10 @@ def get_artist_by_id():
         response = controller.get_artist_by_id(data.get('id'))
         return jsonify(response)
     except Exception as e:
-        return jsonify({'Failed to retrieve artist by id': 500, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to retrieve artist by id',
+            'error': str(e)
+        }), 400
     
 @app.route('/api/artist/name', methods=['GET'])
 def get_artist_by_name():    
@@ -36,7 +48,10 @@ def get_artist_by_name():
         response = controller.get_artist_by_name(data.get('name'))
         return jsonify(response)
     except Exception as e:
-        return jsonify({'Failed to retrieve artist by name': 500, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to retrieve artist by name',
+            'error': str(e)
+        }), 400
 
 @app.route('/api/artist/create', methods=['POST'])
 def create_artist():
@@ -44,11 +59,14 @@ def create_artist():
         data = request.json
         added_artist = controller.add_artist(data.get('name'), data.get('email'))
         if added_artist:
-            return jsonify({'Succssessfully created artist': 200})
+            return jsonify({'message': 'Succssessfully created artist'}), 200
         else:
             raise Exception('Failed to create artist.')
     except Exception as e:
-        return jsonify({'Failed to create artist': 400, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed create artist',
+            'error': str(e)
+        }), 400
     
 @app.route('/api/artist/update', methods=['PUT'])
 def update_artist():
@@ -57,11 +75,14 @@ def update_artist():
         updated_artist = controller.update_artist(int(data.get('id')), data.get('name'), data.get('email'), 
             int(data.get('listeners')), int(data.get('followers')), int(data.get('songs')), int(data.get('albums')))
         if updated_artist:
-            return jsonify({'Succssessfully updated artist': 200})
+            return jsonify({'message': 'Succssessfully updated artist'}), 200
         else:
             raise Exception('Failed to update artist.')
     except Exception as e:
-        return jsonify({'Failed to update artist': 400, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to update artist',
+            'error': str(e)
+        }), 400
     
 @app.route('/api/artist/delete', methods=['DELETE'])
 def delete_artist():
@@ -69,11 +90,14 @@ def delete_artist():
         data = request.json
         deleted_artist = controller.remove_artist(data.get('name'))
         if deleted_artist:
-            return jsonify({'Succssessfully deleted artist': 200})
+            return jsonify({'message': 'Succssessfully deleted artist'}), 200
         else:
-            return jsonify({'Artist not found': 200})
+            return jsonify({'message': 'Artist not found'}), 200
     except Exception as e:
-        return jsonify({'Failed to delete artist': 400, 'Error': str(e)})
+        return jsonify({
+            'message': 'Failed to delete artist',
+            'error': str(e)
+        }), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=6000)
